003fe76d7563fa77e420f22c68b9109c406d7e54
Rmolimock <495@holbertonschool.com>
2019-02-16 22:13:44 +0000

    file_storage save and reload methods need to use [file_path/object.value(objectname)] to open existing or create new files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,28 +23,8 @@ class FileStorage():
         self.__class__.__objects['{}.{}'.format(str(obj), obj.id)] = obj.to_dict()
 
     def save(self):
-        for obj in self.__class__.__objects:
-            filename = ""
-            filename += self.__class__.__file_path
-            filename += '/'
-            filename += obj
-            try:
-                with open(filename, mode="rb+") as file:
-                    pass
-            except FileNotFoundError:
-                with open('{}.py'.format(filename), mode="w") as file:
-                    pass
-
 
     def reload(self):
-        if self.__class__.__objects is not None:
-            for obj in self.__class__.__objects:
-                filename = ""
-                filename += self.__class__.__file_path
-                filename += '/'
-                filename += obj
-                with open(filename, mode="r") as file:
-                    return json.load(file)
 
     @property
     def objects(self):
