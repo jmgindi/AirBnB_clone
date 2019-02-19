79af3ae6237dc57c9ba2f07664739f0082feda18
Andrew Lindburg <atlindburg@gmail.com>
2019-02-19 02:10:26 +0000

    Added basic stuff to filestorage save and reload functions.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage():
     """convert between dictionary string representations of
     objects and JSON files
     """
-    __file_path = ""      # path to the JSON file
+    __file_path = "file.json"      # path to the JSON file
     __objects = {}        # stores objects by [class].[id]
 
     def __init__(self):
@@ -23,8 +23,17 @@ class FileStorage():
         self.__class__.__objects['{}.{}'.format(str(obj), obj.id)] = obj.to_dict()
 
     def save(self):
+        """save"""
+        with open(FileStorage.__file_path, mode="w") as fp:
+            json.dump(FileStorage.objects, fp)
 
     def reload(self):
+        """reload"""
+        try:
+            with open(FileStorage.__file_path) as fp:
+                data = json.load(fp)
+        except:
+            pass
 
     @property
     def objects(self):
